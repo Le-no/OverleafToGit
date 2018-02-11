1e73477a7d235f221ff6d307cbafc17203ba5e76
Josef Ilg <jo-ilg@t-online.de>
2018-02-11 15:26:05 +0100

    init

---
+++ script.bat
@@ -0,0 +1,15 @@
+Rem the specific names for the Github name (XXX) and Overleaf name (YYY)
+
+git clone https://git.overleaf.com/XXX
+git clone https://github.com/YYY.git
+
+cd YYY
+xcopy /s /y ../XXX . 
+git add *
+git commit -m "Security Save from Overleaf"
+git push origin master
+cd ..
+
+RMDIR XXX /S /Q
+RMDIR YYY /S /Q
+set /p ANSWER=Quit? [press key]
